c4b5579f0a2eb6f67aa96cab757c0fa14fab3abe
LiubovAnufriieva <lyuzika@gmail.com>
2024-05-01 20:20:09 +0300

    upd

---
+++ src/js/about-me.js
@@ -0,0 +1,69 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+
+const accordionPanels = document.querySelector('.accordion-panel');
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+const firstContainer = document.querySelector('.about-us');
+const firstIcon = document.querySelector('.mobile-open-us');
+
+accordionPanels.style.maxHeight = '100%';
+firstContainer.classList.add('active');
+firstIcon.style.transform = 'rotate(180deg)';
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', function () {
+    const accordionItem = header.closest('.about-us');
+    const panel = accordionItem.querySelector('.accordion-panel');
+    const icon = header.querySelector('.mobile-open-us');
+
+    accordionItem.classList.toggle('active');
+
+    if (accordionItem.classList.contains('active')) {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+      icon.style.transform = 'rotate(180deg)';
+    } else {
+      panel.style.maxHeight = '0';
+      icon.style.transform = 'rotate(0deg)';
+    }
+  });
+});
+
+// Swiper
+const swiperAbout = new Swiper('.about-swiper-container', {
+  loop: true,
+  setWrapperSize: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  spaceBetween: 0,
+  simulateTouch: true,
+  grabCursor: true,
+  slideActiveClass: 'active',
+
+  navigation: {
+    nextEl: '.about-swiper-button',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    375: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+});
+
+document.querySelector('.about-swiper-button').addEventListener('click', () => {
+  swiperAbout.slideNext();
+});
---
+++ src/js/menu.js
@@ -0,0 +1,52 @@
+// Відкриття Меню
+
+const navList = document.querySelector("nav-list");
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+menuToggle.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
+
+// Відкриття модального вікна
+const backdrop = document.querySelector('.backdrop');
+const burgerIcon = document.querySelector('.burger-icon');
+burgerIcon.addEventListener('click', openMenu);
+
+// Закриття модального вікна
+
+const orderBtn = document.querySelector('.mobile-header-btn');
+const modalMenu = document.querySelector('.mobile-menu-list');
+const closeButton = document.querySelector('.xclose-btn-mob');
+closeButton.addEventListener('click', closeMenu);
+modalMenu.addEventListener('click', closeMenu);
+orderBtn.addEventListener('click', closeMenu);
+
+// Функція для відкриття/зактирря Меню
+function toggleMenu() {
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+    console.log('clickMenu');
+}
+
+// Функція для відкриття модального вікна
+function openMenu() {
+    backdrop.classList.add('is-open');
+    console.log('isOpen');
+
+    window.addEventListener('resize', closeMenuOnResize);
+}
+
+
+// Функція для автоматичного закриття модального вікна при зміні розміру екрану
+function closeMenuOnResize() {
+    if (window.innerWidth > 767) {
+      closeMenu();
+    }
+  }
+  
+  // Функція для закриття модального вікна
+  function closeMenu() {
+    backdrop.classList.remove('is-open');
+  }
