aa039f5255a545cd1ee63d5b08d43d2d01dd6dfc
Lyuzika <lyuzika@gmail.com>
2024-07-05 09:01:54 +0300

    some upd

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 const accordions = Array.from(document.querySelectorAll('.accordion-container'));
-new Accordion(accordions, {duration: 800,
+new Accordion(accordions, 
+  {duration: 800,
     showMultiple: true,
   });
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,97 +1,104 @@
-import axios from 'axios'
+import axios from 'axios';
 import iziToast from 'izitoast';
-import "izitoast/dist/css/iziToast.min.css";
-
+import 'izitoast/dist/css/iziToast.min.css';
 
 import Swiper from 'swiper';
 import 'swiper/css';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
 const swiperReviews = new Swiper('.reviews-wrapper', {
-    modules: [Navigation, Keyboard, Mousewheel],
-    direction: 'horizontal',
-    updateOnWindowResize: true,
-    slidesPerView: 1,
-    enabled: true,
-    swipeHandler: '.reviews-item',
-    speed: 300,
-    spaceBetween: 20,
-    height: 302,
-  
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-  
-    mousewheel: {
-      invert: true,
-    },
-  
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-        slidesPerGroup: 1,
-        spaceBetween: 16,
-      },
-  
-      1440: {
-        slidesPerView: 4,
-        slidesPerGroup: 1,
-        spaceBetween: 16,
-      },
-    },
-  
-    navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
-      preventClicks: false,
-    },
-  });
+  modules: [Navigation, Keyboard, Mousewheel],
+  direction: 'horizontal',
+  updateOnWindowResize: true,
+  slidesPerView: 1,
+  enabled: true,
+  swipeHandler: '.reviews-item',
+  speed: 300,
+  spaceBetween: 20,
+  height: 302,
 
-const BASE_URL = "https://portfolio-js.b.goit.study/api";
-const END_POINT = "/reviews";
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  mousewheel: {
+    invert: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 16,
+    },
+
+    1440: {
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+      spaceBetween: 16,
+    },
+  },
+
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    preventClicks: false,
+  },
+});
+
+const BASE_URL = 'https://portfolio-js.b.goit.study/api';
+const END_POINT = '/reviews';
 
 const url = `${BASE_URL}${END_POINT}`;
 
 let currentId = 1;
 
-
 const selectors = {
-    container: document.querySelector(".reviews-list"),
-    btnPrev: document.querySelector(".swiper-button-prev"),
-    btnNext: document.querySelector(".swiper-button-next")
+  container: document.querySelector('.reviews-list'),
+  btnPrev: document.querySelector('.swiper-button-prev'),
+  btnNext: document.querySelector('.swiper-button-next'),
+};
 
+async function fetchReviews(id) {
+  try {
+    const response = await axios.get(url, { params: { id } });
+
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error(`Fetching reviews failed: ${error.message}`);
+  }
 }
 
+const renderReviews = async () => {
+  try {
+    const reviews = await fetchReviews(currentId);
+    const markup = reviews
+      .map(
+        ({ _id, author, avatar_url, review }) => `
+  
+  <li class="reviews-item swiper-slide" data="${_id}">
+  <img class="reviews-img" src="${avatar_url}" alt="${author}"/>
+  
+  <p class="reviews-text">${review}</p>
+  <h2 class="reviews-subtitle">${author}</h2>
 
-getReviews(currentId)
-.then ((data) => {
-    selectors.container.innerHTML = '';
-    selectors.container.insertAdjacentHTML("beforeend", createMarkup(data))
-                })
-.catch ((error) => {console.log(error);
-iziToast.error({
-    title: 'Error',
-    message: 'Not found',
-});
-})
+  </li>
+  
+  `
+      )
+      .join('');
 
+    selectors.container.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    selectors.container.insertAdjacentHTML(
+      'afterbegin',
+      `<p class="reviews-error-text">Not found</p>`
+    );
+  }
+};
 
-async function getReviews (id) {
-    const { data } = await axios.get (url, {params: { id }})
-    return data;
-}
-
-function createMarkup (arr) {
-    return arr.map (({ _id, author, avatar_url, review }) => `
-    
-    <li class="reviews-item swiper-slide" data="${_id}">
-    <img class="reviews-img" src="${avatar_url}" alt="${author}"/>
-    
-    <p class="reviews-text">${review}</p>
-    <h2 class="reviews-subtitle">${author}</h2>
-
-    </li>
-    
-    `).join ("")
-}
+renderReviews(currentId);
